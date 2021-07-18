abfa4878c8a8f5c05a01726723b0874c051a6917
Andjelko Perisic <66070554+anjeyy@users.noreply.github.com>
2021-07-18 11:15:51 +0200

    initial setup

---
+++ src/main/java/com/github/anjeyy/adventcode/three/Main.java
@@ -0,0 +1,25 @@
+package com.github.anjeyy.adventcode.three;
+
+import java.io.IOException;
+
+class Main {
+
+    private Main() {
+        throw new UnsupportedOperationException("No instance allowed");
+    }
+
+    static void main(String[] args) throws IOException {
+
+        solvePartOne();
+
+        solvePartTwo();
+    }
+
+    private static void solvePartOne() throws IOException {
+        //todo
+    }
+
+    private static void solvePartTwo() throws IOException {
+        //todo
+    }
+}
