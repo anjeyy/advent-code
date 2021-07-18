415bc247b3c475ca6b5ffb79f990684b7e2c2454
Andjelko Perisic <andjelko.perisic@gmail.com>
2021-07-18 11:12:27 +0200

    Day 2 solution (#1)

---
+++ src/main/java/com/github/anjeyy/adventcode/two/Main.java
@@ -0,0 +1,42 @@
+package com.github.anjeyy.adventcode.two;
+
+import com.github.anjeyy.adventcode.AdventFileReader;
+import java.io.IOException;
+
+class Main {
+
+    static final String WHITESPACE = " ";
+
+    static void main(String[] args) throws IOException {
+
+        solvePartOne();
+
+        solvePartTwo();
+    }
+
+    private static void solvePartOne() throws IOException {
+        long validPasswords =
+            AdventFileReader.readInputAsStringList("two_password-list.txt")
+                            .stream()
+                            .filter(AdventFileReader.stringIsNotBlank())
+                            .map(String::trim)
+                            .map(PasswordParser::from)
+                            .filter(PasswordParser::isValidPartOne)
+                            .count();
+
+        System.out.println("Part I: " + validPasswords);
+    }
+
+    private static void solvePartTwo() throws IOException {
+        long validPasswords =
+            AdventFileReader.readInputAsStringList("two_password-list.txt")
+                            .stream()
+                            .filter(AdventFileReader.stringIsNotBlank())
+                            .map(String::trim)
+                            .map(PasswordParser::from)
+                            .filter(PasswordParser::isValidPartTwo)
+                            .count();
+
+        System.out.println("Part II: " + validPasswords);
+    }
+}
