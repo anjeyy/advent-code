479c9179901c4667c0d04f3c18901c4b4acac2d5
Andjelko Perisic <66070554+anjeyy@users.noreply.github.com>
2021-08-08 21:50:41 +0200

    solved part 2

--- src/main/java/com/github/anjeyy/adventcode/six/Main.java
+++ src/main/java/com/github/anjeyy/adventcode/six/Main.java
@@ -18,7 +18,11 @@ class Main {
 
     private static void solvePartOne() throws IOException {
         int uniqueAnswered = Arrays
-            .stream(AdventFileReader.readInputAsString("six_customs-list.txt").split("\n\n"))
+            .stream(
+                AdventFileReader
+                    .readInputAsString("six_customs-list.txt")
+                    .split(System.lineSeparator() + System.lineSeparator())
+            )
             .map(String::trim)
             .map(CustomsGroup::from)
             .map(CustomsGroup::uniqueQuestionAnswered)
@@ -28,32 +32,17 @@ class Main {
     }
 
     private static void solvePartTwo() throws IOException {
-        //        Map<Integer, Seat> seatList = AdventFileReader
-        //            .readInputAsStringList("six_customs-list.txt")
-        //            .stream()
-        //            .map(String::trim)
-        //            .map(SeatParser::from)
-        //            .map(s -> Seat.of(s.determineRow(), s.determineColumn()))
-        //            .collect(Collectors.toMap(Seat::getSeatId, Function.identity()));
-        //
-        //        int maxSeatId = 127 * 8 + 7; // 1023
-        //        List<Integer> potentialSeats = new ArrayList<>();
-        //        for (int i = 1; i < maxSeatId - 1; i++) {
-        //            Seat currentSeat = seatList.get(i);
-        //            if (currentSeat == null) {
-        //                Seat previousSeat = seatList.get(i - 1);
-        //                if (previousSeat != null) {
-        //                    Seat followingSeat = seatList.get(i + 1);
-        //                    {
-        //                        if (followingSeat != null) {
-        //                            potentialSeats.add(i);
-        //                        }
-        //                    }
-        //                }
-        //            }
-        //        }
-        //
-        //        System.out.println("Part II: " + maxSeatId);
+        int commonAnswered = Arrays
+            .stream(
+                AdventFileReader
+                    .readInputAsString("six_customs-list.txt")
+                    .split(System.lineSeparator() + System.lineSeparator())
+            )
+            .map(String::trim)
+            .map(CustomsGroup::from)
+            .map(CustomsGroup::commonQuestionAnswered)
+            .reduce(0, Integer::sum);
 
+        System.out.println("Part II: " + commonAnswered);
     }
 }
