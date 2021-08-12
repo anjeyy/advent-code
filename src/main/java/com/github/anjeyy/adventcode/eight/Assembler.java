e41d7c4d53d48a42ea410aca3314e53455f60fd2
Andjelko Perisic <66070554+anjeyy@users.noreply.github.com>
2021-08-12 20:44:56 +0200

    refactor part 1

--- src/main/java/com/github/anjeyy/adventcode/eight/Assembler.java
+++ src/main/java/com/github/anjeyy/adventcode/eight/Assembler.java
@@ -11,6 +11,7 @@ import java.util.Map.Entry;
 class Assembler {
 
     private final Map<Instruction, Boolean> instructions;
+    private final ListIterator<Entry<Instruction, Boolean>> instructionsIterator;
 
     static Assembler from(List<Instruction> instructions) {
         return new Assembler(instructions);
@@ -18,6 +19,7 @@ class Assembler {
 
     Assembler(List<Instruction> instructions) {
         this.instructions = initOrderedMap(instructions);
+        this.instructionsIterator = new ArrayList<>(this.instructions.entrySet()).listIterator();
     }
 
     private static Map<Instruction, Boolean> initOrderedMap(List<Instruction> instructions) {
@@ -27,50 +29,68 @@ class Assembler {
     }
 
     int execute() {
-        ListIterator<Entry<Instruction, Boolean>> entryIterator =
-            new ArrayList<>(instructions.entrySet()).listIterator();
         boolean instructionIsUnique = true;
         int result = 0;
         while (instructionIsUnique) {
-            if (!entryIterator.hasNext()) {
+            if (!instructionsIterator.hasNext()) {
                 break;
             }
 
-            Map.Entry<Instruction, Boolean> currentEntry = entryIterator.next();
+            Map.Entry<Instruction, Boolean> currentEntry = instructionsIterator.next();
             if (currentEntry.getValue()) {
                 instructionIsUnique = false;
             } else {
                 currentEntry.setValue(true);
                 Instruction currentInstruction = currentEntry.getKey();
                 Operation currentOperation = currentInstruction.getOperation();
-                int argument = currentInstruction.getArgument();
-                if (currentOperation == Operation.NO_OPERATION) {
-                    // do nothing
-                } else if (currentOperation == Operation.ACCUMULATOR) {
-                    result = result + argument;
-                } else if (currentOperation == Operation.JUMPS) {
-                    if (argument < 0) { // negative jumps
-                        for (int i = 0; i >= argument; i--) {
-                            if (entryIterator.hasPrevious()) {
-                                entryIterator.previous();
-                            } else {
-                                throw new IllegalStateException("Jumped too high.");
-                            }
-                        }
-                    } else if (argument > 0) {  // positive jumps
-                        for (int i = 1; i < argument; i++) {
-                            if (entryIterator.hasNext()) {
-                                entryIterator.next();
-                            } else {
-                                throw new IllegalStateException("Jumped too high.");
-                            }
-                        }
-                    } else {
-                        throw new IllegalStateException("Jump does not support '0'.");
-                    }
-                }
+                final int argument = currentInstruction.getArgument();
+                result = selectOperation(currentOperation, result, argument);
             }
         }
         return result;
+    }
+
+    private int selectOperation(Operation operation, int result, int value) {
+        switch (operation) {
+            case NO_OPERATION:
+                return result;
+            case ACCUMULATOR:
+                return result + value;
+            case JUMPS:
+                executeJump(value);
+                return result;
+            default:
+                throw new IllegalStateException("Jump does not support '0'.");
+        }
+    }
+
+    private void executeJump(int jumps) {
+        if (jumps < 0) {
+            jumpToPreviousPosition(jumps);
+        } else if (jumps > 0) {
+            jumpToNextPosition(jumps);
+        } else {
+            throw new IllegalStateException("Jump does not support '0'.");
+        }
+    }
+
+    private void jumpToPreviousPosition(int jumps) {
+        for (int i = 0; i >= jumps; i--) {
+            if (instructionsIterator.hasPrevious()) {
+                instructionsIterator.previous();
+            } else {
+                throw new IllegalStateException("Jumped too high.");
+            }
+        }
+    }
+
+    private void jumpToNextPosition(int jumps) {
+        for (int i = 1; i < jumps; i++) {
+            if (instructionsIterator.hasNext()) {
+                instructionsIterator.next();
+            } else {
+                throw new IllegalStateException("Jumped too low.");
+            }
+        }
     }
 }
